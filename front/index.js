5ba8da0a51b5957ce13968ce2abf5f5ac428fa3a
AlexKirovich <alex.k.kirovich@gmail.com>
2022-04-07 18:55:25 +0300

    app to test user input and displaying it on page

--- front/index.js
+++ front/index.js
@@ -1,1 +1,25 @@
-console.log('hahah')
+const form = document.querySelector("form");
+const input = document.querySelector("[name='input']");
+const todoList = document.getElementById("list");
+
+const existingTodos = JSON.parse(localStorage.getItem("list")) || [];
+
+const todoData = [];
+
+existingTodos.forEach((todo) => {
+  addTodo(todo);
+});
+function addTodo(todoText) {
+  todoData.push(todoText);
+  const li = document.createElement("li");
+  li.innerHTML = todoText;
+  todoList.appendChild(li);
+  localStorage.setItem("list", JSON.stringify(todoData));
+  input.value = "";
+}
+
+// Events
+form.onsubmit = (event) => {
+  event.preventDefault();
+  addTodo(input.value);
+};
